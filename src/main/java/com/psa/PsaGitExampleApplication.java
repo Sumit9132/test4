b2462058d10db4154fcbb2d5e0c6c673055a95fb
Sumit Kumar <142964175+Sumit9132@users.noreply.github.com>
2025-03-08 19:32:01 +0530

    Update PsaGitExampleApplication.java
    my first change

--- src/main/java/com/psa/PsaGitExampleApplication.java
+++ src/main/java/com/psa/PsaGitExampleApplication.java
@@ -8,6 +8,7 @@ public class PsaGitExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PsaGitExampleApplication.class, args);
+		System.out.println("hello changes");
 	}
 
 }
